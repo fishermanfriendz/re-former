d233cb336235f805331888ed471431d340584320
CodePioneer2 <166389859+CodePioneer2@users.noreply.github.com>
2025-01-25 20:27:18 +0200

    Small fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "users#new"
   resources :users, only: [ :new, :create, :edit, :update ]
 end
